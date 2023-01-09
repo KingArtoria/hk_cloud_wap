4f600b22a55b758d25b30e1b4d5e702b24824948
KingArtoria <gaojiuan@163.com>
2023-01-09 14:05:59 +0800

    2

---
+++ src/components/Material.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <!-- 盒子 -->
+    <div class="material">
+      <!-- 头部 -->
+      <div class="material_1">
+        <!-- 左侧图片 -->
+        <img src="" class="material_1_1" />
+        <!-- 右侧文案 -->
+        <div class="material_1_2"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 const routes = [
-  {
-    path: '/', name: 'Home', component: () => import('../views/index/index.vue')
-  }
+  { path: '/', name: 'Home', component: () => import('../views/index/index.vue') },
+  { path: '/material', name: 'Material', component: () => import('../views/material/index.vue') },
 ]
 const router = new VueRouter({
   routes,
